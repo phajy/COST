2689637f59a6715a7303f41cc02d879747d826c1
Caitlin Coss <fb22779@bristol.ac.uk>
2026-01-30 01:12:28 +0000

    update

--- src/LineProfileKerr.jl
+++ src/LineProfileKerr.jl
@@ -1,21 +1,20 @@
-using Gradus 
-using StaticArrays 
-using Plots # Kerr black hole (G = c = M = 1 units) 
-m = KerrMetric(M = 1.0, a = 0.998) 
+using Gradus
+using StaticArrays
+using Plots
 
-# Infinitely thin equatorial disc
+m = KerrMetric(M = 1.0, a = 0.998)
 
-d = ThinDisc(0.0, Inf) 
-# Observer position: # (t, r, θ, φ) 
+d = ThinDisc(0.0, Inf)
+
 x = SVector(0.0, 10_000.0, deg2rad(60.0), 0.0)
 
-bins, flux = lineprofile(m, x, d) 
+bins, flux = lineprofile(m, x, d)
 
-plot( 
-    bins, 
-    flux, 
-    xlabel = "g = E / E₀", 
-    ylabel = "Flux", 
-    legend = false, 
-    lw = 2 
+plot(
+    bins,
+    flux,
+    xlabel = "g = E / E₀",
+    ylabel = "Flux",
+    legend = false,
+    lw = 2
 )
