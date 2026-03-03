1f315a9cd080f327e56d416a201d6e972aed06d7
Caitlin Coss <fb22779@bristol.ac.uk>
2026-03-03 23:06:12 +0000

    Inclination Angle

---
+++ src/LineProfileIncl.jl
@@ -0,0 +1,25 @@
+using Gradus
+using StaticArrays
+using Plots
+
+inclination = [20.0, 40.0, 60.0, 70.0, 80.0]
+m = KerrMetric(M = 1.0, a = 0.998)
+
+d = ThinDisc(0.0, Inf)
+
+
+plt = Plots.plot(
+    xlabel = "E / E₀",
+    ylabel = "Flux",
+    lw = 2
+)
+
+for inc in inclination
+    x = SVector(0.0, 10_000.0, deg2rad(inc), 0.0)
+    bins, flux = lineprofile(m, x, d)
+    Plots.plot!(plt, bins, flux, label = "Inclination angle = $inc")
+end
+
+plt
+
+savefig("docs/src/figures/LineKerrIncl.png")
