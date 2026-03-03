62ad383345721d24e11e2da9d01335455a8333e3
Caitlin Coss <fb22779@bristol.ac.uk>
2026-03-03 16:51:49 +0000

    Line profiles

---
+++ src/JohannsenLine.jl
@@ -0,0 +1,21 @@
+using Gradus
+using StaticArrays
+using Plots
+
+m = JohannsenPsaltisMetric(M = 1.0, a = 0.8, ϵ3 = 0.5)
+
+d = ThinDisc(0.0, Inf) #defines the accretion disk. Gradus will start with the emission at the ISCO and extends to infinity
+
+x = SVector(0.0, 10_000.0, deg2rad(60.0), 0.0)#defines the observers position in spacetime (t, r, θ, ϕ)
+
+bins, flux = lineprofile(m, x, d)
+
+fig4 = Plots.plot(
+    bins,
+    flux,
+    xlabel = "g = E / E₀",
+    ylabel = "Flux",
+    legend = false,
+    lw = 2
+)
+
