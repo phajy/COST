7fe03a3cea7dc312f6d8640c39eaeaaa75342764
Caitlin Coss <fb22779@bristol.ac.uk>
2026-03-31 17:28:16 +0100

    commit

--- src/L_against_r.jl
+++ src/L_against_r.jl
@@ -9,7 +9,7 @@ r = range(3.1*M, 20M, length=500)
 L = sqrt.(M * r.^2 ./ (r .- 3*M))
 
 # Plot L vs r
-plot(r, L, xlabel="r", ylabel="L", label ="Angular Momentum for Circular Orbit", lw=2, color=:blue)
+plot(r, L, xlabel="r", ylabel="L", label ="Angular Momentum for Circular Orbit", lw=2, color=:blue, legendfontsize=12)
 
 # Add vertical dotted line for ISCO at r = 6M
 vline!([6M], linestyle=:dot, lw=2, color=:red, label="ISCO (r = 6M)")
